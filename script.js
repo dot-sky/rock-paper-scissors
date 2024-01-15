911741c9a3d150104013073b602f8dec2d433c0a
dot-sky <108539845+dot-sky@users.noreply.github.com>
2024-01-15 08:23:33 -0400

    Change game round logic, update functions
    The result of round evaluation now returns numbers (0 = tie, 1 = player, 2 = computer)

--- script.js
+++ script.js
@@ -17,34 +17,63 @@ function getComputerChoice(){
 function getRandomNumber(upperLimit){
     return Math.floor(Math.random()*upperLimit) + 1;
 }
-function evaluateChoices(playerSelection, computerSelection){
-    playerSelection = capitalize(playerSelection);
-    console.log("You've selected " + playerSelection)
-    if (playerSelection === computerSelection){
-        return "It's a tie!";
-    }
-    else if ((playerSelection === "Rock" && computerSelection === "Scissors")
-            || (playerSelection === "Scissors" && computerSelection === "Paper")
-            || (playerSelection === "Paper" && computerSelection === "Rock")){
-        return "You win! " +playerSelection+ " beats " + computerSelection;
-    }
-    else
-        return "You Lose! " +computerSelection+ " beats " + playerSelection;
-
-}
 function capitalize(text){
     if (text === "")
         return text;
     return text.at(0).toUpperCase() + text.slice(1).toLowerCase();
 }
-function gameRound(){
-    let playerSelection = prompt("Your choice!");
-    const computerSelection = "Rock";
-    const result = evaluateChoices(playerSelection, computerSelection);
-    console.log(result);
-    if (result === "It's a tie!"){
-        console.log("Both players selected " + playerSelection + ". Play again...");
-        gameRound();
+function evaluateChoices(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return 0;
+    }
+    else if ((playerSelection === "Rock" && computerSelection === "Scissors")
+            || (playerSelection === "Scissors" && computerSelection === "Paper")
+            || (playerSelection === "Paper" && computerSelection === "Rock")){
+        return 1;
+    }
+    else{
+        return 2;
     }
 }
-gameRound();
+function gameRound(){
+    let result = 0, playerSelection, computerSelection;
+    do {
+        playerSelection = capitalize(prompt("Your choice!"));
+        computerSelection = getComputerChoice();
+        result = evaluateChoices(playerSelection, computerSelection);
+        if (!result){
+            console.log("It's a tie!");
+            console.log("Both players selected " + computerSelection + ". Play again...");
+        }
+    } while (!result)
+    if (result === 1){
+        console.log("You win! " +playerSelection+ " beats " + computerSelection);
+    }
+    else {
+        console.log("You Lose! " +computerSelection+ " beats " + playerSelection);
+    }
+    return result;
+}
+function game(){
+    let playerPoints = 0;
+    let computerPoints = 0;
+    for (let round = 1; round <= 5; round++) {
+        console.log("---- Round: " + round + " ----");
+        if (gameRound() === 1)
+            playerPoints++;
+        else
+            computerPoints++;
+    }
+    console.log("");
+    console.log("==== The final winner is..." + " ====");
+    if (playerPoints > computerPoints){
+        console.log("You!!!");
+    }
+    else{
+        console.log("The computer!");
+    }
+    console.log(" > Player points: "+ playerPoints);
+    console.log(" > Computer points: "+ computerPoints);
+}
+game();
+  
